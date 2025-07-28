350f0702ef4dc78bd00eee4b72eec545d53da110
Jon Cruxible <tagrous87@gmail.com>
2025-07-27 21:59:55 -0400

    main_cyon.c

--- src/main_cyon.c
+++ src/main_cyon.c
@@ -1,102 +1,133 @@
+#include <gtk/gtk.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdio.h>
-#include "../include/downloader.h"
+#include <libgen.h>
+#include <limits.h>
+#include <string.h>
+#include "downloader.h"  // Ensure this header defines show_downloader_window()
 
-// Function to launch cyon_cli as a separate process
-void run_cyon_cli() {
+// Launch cyon_cli in a terminal
+void launch_cli(GtkWidget *widget, gpointer data) {
     const char *terminals[] = {
-        "mate-terminal",
-        "gnome-terminal",
-        "xfce4-terminal",
-        "xterm",
-        NULL
+        "mate-terminal", "x-terminal-emulator", "gnome-terminal",
+        "xfce4-terminal", "lxterminal", "konsole", "xterm"
     };
 
-    for (int i = 0; terminals[i] != NULL; ++i) {
-        pid_t pid = fork();
-
-        if (pid == 0) {
-            // Child process
-            setsid();  // Optional: new session
-            execlp(terminals[i], terminals[i], "-e", "./cyon_cli", (char *)NULL);
-            _exit(1);  // exec failed
-        } else if (pid > 0) {
-            int status;
-            waitpid(pid, &status, 0);
-
-            if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
-                return;  // Success
-            }
-        } else {
-            perror("fork failed");
-        }
+    char full_path[PATH_MAX];
+    ssize_t len = readlink("/proc/self/exe", full_path, sizeof(full_path) - 1);
+    if (len == -1) {
+        perror("readlink");
+        return;
     }
 
-    fprintf(stderr, "Could not launch cyon_cli in any terminal.\n");
+    full_path[len] = '\0';
+    char *dir = dirname(full_path);
+
+    char cli_path[PATH_MAX];
+    snprintf(cli_path, sizeof(cli_path), "%s/cyon_cli", dir);
+
+    for (int i = 0; i < sizeof(terminals)/sizeof(terminals[0]); i++) {
+        char cmd[PATH_MAX + 64];
+        snprintf(cmd, sizeof(cmd), "%s -e '%s'", terminals[i], cli_path);
+        if (system(cmd) == 0) return;
+    }
+
+    g_print("No compatible terminal found.\n");
 }
 
-// Callback for menu item to open the downloader window
-static void on_open_downloader(GtkWidget *widget, gpointer data) {
-    show_downloader_window();  // Defined in downloader.c
+// Dummy Tools handler
+void tool_a(GtkWidget *widget, gpointer data) {
+    g_print("Tool A selected.\n");
 }
 
-// Callback to run the CLI tool
-static void on_run_cli(GtkWidget *widget, gpointer data) {
-    run_cyon_cli();  // Calls the real launcher
+void tool_b(GtkWidget *widget, gpointer data) {
+    g_print("Tool B selected.\n");
+}
+
+// Dummy Security handler
+void firewall_sec(GtkWidget *widget, gpointer data) {
+    g_print("Firewall selected.\n");
+}
+
+void scanner_sec(GtkWidget *widget, gpointer data) {
+    g_print("Scanner selected.\n");
+}
+
+// Downloader handler
+void open_downloader(GtkWidget *widget, gpointer data) {
+    show_downloader_window();  // Function from downloader.c
 }
 
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
     // Create main window
-    GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(win), "Cyon");
-    gtk_window_set_default_size(GTK_WINDOW(win), 500, 500);
-    g_signal_connect(win, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Cyon");
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // Main container
+    // Vertical layout
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    gtk_container_set_border_width(GTK_CONTAINER(vbox), 10);
-    gtk_container_add(GTK_CONTAINER(win), vbox);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
 
-    // Create menu bar
-    GtkWidget *menu_bar = gtk_menu_bar_new();
+    // Menu bar
+    GtkWidget *menubar = gtk_menu_bar_new();
 
-    // "Tools" menu
-    GtkWidget *tools_menu = gtk_menu_new();
+    // === Programs Menu ===
+    GtkWidget *programs_menu = gtk_menu_new();
+    GtkWidget *programs_item = gtk_menu_item_new_with_label("Programs");
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(programs_item), programs_menu);
+
+    // CLI Terminal
+    GtkWidget *cli_item = gtk_menu_item_new_with_label("CLI Terminal");
+    gtk_menu_shell_append(GTK_MENU_SHELL(programs_menu), cli_item);
+    g_signal_connect(cli_item, "activate", G_CALLBACK(launch_cli), NULL);
+
+    // Downloader
+    GtkWidget *downloader_item = gtk_menu_item_new_with_label("Downloader");
+    gtk_menu_shell_append(GTK_MENU_SHELL(programs_menu), downloader_item);
+    g_signal_connect(downloader_item, "activate", G_CALLBACK(open_downloader), NULL);
+
+    // === Tools Submenu ===
     GtkWidget *tools_item = gtk_menu_item_new_with_label("Tools");
+    GtkWidget *tools_menu = gtk_menu_new();
     gtk_menu_item_set_submenu(GTK_MENU_ITEM(tools_item), tools_menu);
 
-    // "Open Downloader" menu item
-    GtkWidget *open_item = gtk_menu_item_new_with_label("Open Downloader");
-    g_signal_connect(open_item, "activate", G_CALLBACK(on_open_downloader), NULL);
-    gtk_menu_shell_append(GTK_MENU_SHELL(tools_menu), open_item);
+    GtkWidget *tool_subitem1 = gtk_menu_item_new_with_label("Tool A");
+    GtkWidget *tool_subitem2 = gtk_menu_item_new_with_label("Tool B");
+    gtk_menu_shell_append(GTK_MENU_SHELL(tools_menu), tool_subitem1);
+    gtk_menu_shell_append(GTK_MENU_SHELL(tools_menu), tool_subitem2);
+    gtk_menu_shell_append(GTK_MENU_SHELL(programs_menu), tools_item);
 
-    // "Open Cyon CLI" menu item
-    GtkWidget *open_item_cli = gtk_menu_item_new_with_label("Open Cyon CLI");
-    g_signal_connect(open_item_cli, "activate", G_CALLBACK(on_run_cli), NULL);
-    gtk_menu_shell_append(GTK_MENU_SHELL(tools_menu), open_item_cli);
+    g_signal_connect(tool_subitem1, "activate", G_CALLBACK(tool_a), NULL);
+    g_signal_connect(tool_subitem2, "activate", G_CALLBACK(tool_b), NULL);
 
-    // Add tools menu to menu bar
-    gtk_menu_shell_append(GTK_MENU_SHELL(menu_bar), tools_item);
+    // === Security Submenu ===
+    GtkWidget *security_item = gtk_menu_item_new_with_label("Security");
+    GtkWidget *security_menu = gtk_menu_new();
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(security_item), security_menu);
 
-    // Add menu bar to the container
-    gtk_box_pack_start(GTK_BOX(vbox), menu_bar, FALSE, FALSE, 0);
+    GtkWidget *security_subitem1 = gtk_menu_item_new_with_label("Firewall");
+    GtkWidget *security_subitem2 = gtk_menu_item_new_with_label("Scanner");
+    gtk_menu_shell_append(GTK_MENU_SHELL(security_menu), security_subitem1);
+    gtk_menu_shell_append(GTK_MENU_SHELL(security_menu), security_subitem2);
+    gtk_menu_shell_append(GTK_MENU_SHELL(programs_menu), security_item);
 
-    // Add a label for aesthetics
-    GtkWidget *label = gtk_label_new("Welcome to Cyon!");
+    g_signal_connect(security_subitem1, "activate", G_CALLBACK(firewall_sec), NULL);
+    g_signal_connect(security_subitem2, "activate", G_CALLBACK(scanner_sec), NULL);
+
+    // Attach menubar
+    gtk_menu_shell_append(GTK_MENU_SHELL(menubar), programs_item);
+    gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
+
+    // Welcome label
+    GtkWidget *label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), "<span font='20'>Welcome to Cyon!</span>");
     gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 10);
 
-    // Show everything
-    gtk_widget_show_all(win);
+    gtk_widget_show_all(window);
     gtk_main();
 
     return 0;
 }
-
-
-
-
-
